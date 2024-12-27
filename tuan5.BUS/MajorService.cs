6191160bc9376ba6b223e3622e4e0486d4becc3e
quangminhnhat <quangminhnhat684@gmail.com>
2024-12-27 17:37:49 +0700

    Add files via upload

---
+++ tuan5.BUS/MajorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tuan5.DAL.Database;
+
+namespace tuan5.BUS
+{
+    public class MajorService
+    {
+        public List<Major> GetAllByFaculty(int facultyId)
+        {
+            StudentDB context = new StudentDB();
+            return context.Majors.Where(p=>p.FacultyID == facultyId).ToList();
+        }
+    }
+}
---
+++ tuan5.BUS/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tuan5.DAL.Database;
+
+namespace tuan5.BUS
+{
+    public class StudentService
+    {
+
+        public List<Student> GetAll()
+        {
+            StudentDB context = new StudentDB();
+            return context.Students.ToList();
+        }
+        public List<Student> GetAllHasNoMajor()
+        {
+            StudentDB context = new StudentDB();
+            return context.Students.Where(p=>p.MajorID == null).ToList();
+        }
+        
+        public List<Student> GetAllHasNoMajor(int facultyID)
+        {
+            StudentDB context = new StudentDB();
+            return context.Students.Where(p => p.MajorID == null && p.FacultyID == facultyID).ToList();
+        }
+        
+        public Student FindById(string studentId)
+        {
+            StudentDB context = new StudentDB();
+            return context.Students.FirstOrDefault(p => p.StudentID == studentId);
+        }
+        public void InsertUpdate(Student s)
+        {
+            StudentDB context = new StudentDB(); 
+            context.Students.AddOrUpdate(s);
+            context.SaveChanges();
+        }        
+    }
+}
